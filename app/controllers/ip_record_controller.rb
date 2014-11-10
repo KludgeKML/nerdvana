d8bf540d7381affcd63ef2a0d7e84b0d13c81158
Keith M Lawrence <keith@kludge.co.uk>
2014-11-10 21:58:32 +0000

    fix controller

--- app/controllers/ip_record_controller.rb
+++ app/controllers/ip_record_controller.rb
@@ -1,13 +1,16 @@
 class IpRecordController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def get_ip
-    @ip_address = IPRecord.first()
+    @ip_address = IpRecord.first
   end
 
   def set_ip
-    redirect_to(action: :get_ip) unless params[:key] == ENV['SET_KEY']
-    @ip_address = IPRecord.first()
-    @ip_address.address = request.remote_ip.to_s
-    @ip_address.save!
+    if params[:key] == ENV['SET_KEY']
+      @ip_address = IpRecord.first_or_create
+      @ip_address.address = request.remote_ip.to_s
+      @ip_address.save!
+    end
     redirect_to(action: :get_ip)
   end
 end
