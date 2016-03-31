c983cd1473af1859d0015b7bb05dbdbc431d596e
Keith M Lawrence <keith@kludge.co.uk>
2016-03-31 12:06:19 +0100

    add sofa route

--- app/controllers/nerdvana_controller.rb
+++ app/controllers/nerdvana_controller.rb
@@ -1,4 +1,8 @@
 class NerdvanaController < ApplicationController
   def index
   end
+
+  def sofa
+    @arm = params[:arm] if params[:arm] == 'left' || params[:arm] == 'right'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Nerdvana::Application.routes.draw do
   get 'nerdvana/index'
 
+  get 'things/sofa(/:arm)' => 'nerdvana#sofa'
+
   get 'latest_ip' => 'ip_record#get_ip'
   post 'latest_ip' => 'ip_record#set_ip'
 
